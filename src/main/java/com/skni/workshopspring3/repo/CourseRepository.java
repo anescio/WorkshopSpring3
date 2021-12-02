f2f79a378ae4eb26c71c8ebf725090866f73fc88
s21193 <s21193@pjwstk.edu.pl>
2021-12-02 17:51:20 +0100

    Created CourseRepository and StudentRepository with basic queries

---
+++ src/main/java/com/skni/workshopspring3/repo/CourseRepository.java
@@ -0,0 +1,13 @@
+package com.skni.workshopspring3.repo;
+
+import com.skni.workshopspring3.repo.entity.Course;
+import com.skni.workshopspring3.repo.entity.CourseTypeEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findAllByCourseType(CourseTypeEnum courseType);
+}
---
+++ src/main/java/com/skni/workshopspring3/repo/StudentRepository.java
@@ -0,0 +1,22 @@
+package com.skni.workshopspring3.repo;
+
+import com.skni.workshopspring3.repo.entity.CourseTypeEnum;
+import com.skni.workshopspring3.repo.entity.GenderEnum;
+import com.skni.workshopspring3.repo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query(value = "SELECT * FROM Student s JOIN Course c ON s.course_id = c.id WHERE s.gender = ?1 AND c.courseType = ?2", nativeQuery = true)
+    List<Student> findAllByGenderAndCourseType(GenderEnum gender, CourseTypeEnum courseType);
+
+   // @Query(value = "SELECT * FROM Student s", nativeQuery = true)
+    List<Student> findALL();
+
+    List<Student> findAllBySurname(String surname);
+}
